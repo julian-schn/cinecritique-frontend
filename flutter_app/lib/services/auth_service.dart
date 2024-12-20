10f8a7d46ab5ad196ac7aabf0179382338573d11
julian <js490@hdm-stuttgart.de>
2024-12-20 12:48:53 +0100

    added keylocak parameters

---
+++ flutter_app/lib/services/auth_service.dart
@@ -0,0 +1,7 @@
+class kc_params {
+  static const String URL = "http://localhost:8080"; //TODO: Server URL
+  static const String REALM = "movie-app";
+  static const String CLIENT = "movie-app-client-frontend";
+}
+
+//TODO: Barrel file

495a0a2a3763b0cf84201415ff283f5a6ae83af9
julian <js490@hdm-stuttgart.de>
2024-12-22 22:50:37 +0100

    changed auth service

--- flutter_app/lib/services/auth_service.dart
+++ flutter_app/lib/services/auth_service.dart
@@ -1,5 +1,5 @@
 class kc_params {
-  static const String URL = "http://localhost:8080"; //TODO: Server URL
+  static const String URL = "cinecritique.mi.hdm-stuttgart.de:8080"; //TODO: Server URL
   static const String REALM = "movie-app";
   static const String CLIENT = "movie-app-client-frontend";
 }
